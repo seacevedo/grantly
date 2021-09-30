71459cc4097fe48eae8fa8e5bade65e552858226
unknown <saulacevedo2.0@gmail..com>
2021-09-30 16:15:11 -0500

    First commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,7 +8,7 @@ module.exports = {
   networks: {
     rinkeby: {
       url: "https://rinkeby.infura.io/v3/8fd146ee992e4360b83c46e7c8166c58", //Infura url with projectId
-      accounts: ["d95610817c7ef2549aafa89f40053612f60e1f152daf7ca60ebe362e55392880"] // add the account that will deploy the contract (private key)
+      accounts: [process.env.REACT_APP_PRIVATE_KEY] // add the account that will deploy the contract (private key)
     },
   }
 };
